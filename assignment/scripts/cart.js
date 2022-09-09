5a38191eb195fd0f1c965f8ec15ea48355d2d6e4
Lydia Wildes <lydiawildes1@gmail.com>
2022-09-09 17:01:02 -0500

    finished stretch, added missing semicolons... sigh.

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,7 +4,7 @@ console.log('***** Cart Functions *****');
 
 //setting basket to empty array
 let basket =[];
-console.log(`expect empty basket`, basket)
+console.log(`expect empty basket`, basket);
 
 //creating function called addItem
 function addItem(item){
@@ -12,10 +12,10 @@ function addItem(item){
    return true;
 
 }
-console.log('Adding carrot: expect true.', addItem('carrot'))
-console.log('Adding pear: expect true.', addItem('pear'))
-console.log('Adding apple: expect true.',addItem('apple'))
-console.log( `Basket is now ${basket}`)
+console.log('Adding carrot: expect true.', addItem('carrot'));
+console.log('Adding pear: expect true.', addItem('pear'));
+console.log('Adding apple: expect true.',addItem('apple'));
+console.log( `Basket is now ${basket}`);
 
 //creating loop in function called listItems
 function listItems(things){
@@ -29,14 +29,14 @@ listItems(basket)
 
 
 //creating function to empty 
-testArray = [1,2,3,]  
+testArray = [1,2,3,]  ;
 function empty(array){
 if(array.length > 0){
     array.length = 0;
-    return array
+    return array;
 }
 }
-console.log('expect to see empty array', empty(testArray))
+console.log('expect to see empty array', empty(testArray));
 
 //Stretch part 1 
 
@@ -46,10 +46,10 @@ const maxItems = 5
 //is full function
 function isFull(array){
     if (array.length < maxItems){
-        return false
+        return false;
     }
     else{
-        return true
+        return true;
     }
 }
 console.log('logging basket, and expect false:', isFull(basket))
@@ -57,17 +57,30 @@ console.log('logging basket, and expect false:', isFull(basket))
 function addItemUpdated(item){
    if(basket.length < maxItems) {
     basket.push(item)
-    return 'another item added';}
+    return 'another item added';
+}
    else {
-    return 'all out of space'
+    return 'all out of space';
     //I chose to do a string that makes more sense to me then "true" or "false" in this scenario. I hope that's ok!
    }   
 }
 
-console.log('logging addItemUpdated and expect "another item added".' , addItemUpdated('orange'))
-console.log('checking contents of basket' ,basket)
-console.log('expect room.', addItemUpdated('cherry'))
-console.log('expect is full.' ,addItemUpdated('coconut'))
-console.log('checking contents of basket' ,basket)
+console.log('logging addItemUpdated and expect "another item added".' , addItemUpdated('orange'));
+console.log('checking contents of basket' ,basket);
+console.log('expect room.', addItemUpdated('cherry'));
+console.log('expect is full.' ,addItemUpdated('coconut'));
+console.log('checking contents of basket' ,basket);
 
 //Stretch part 2
+//creating removeItem function
+
+function removeItem(array, item) {
+    const index = array.indexOf(item);
+  if(index >= 0){
+    return  array.splice(index, 1) ;
+  }
+  else {
+    return null;
+  }
+}
+console.log(removeItem(basket, 'pear'));
